456766c9235c44aed2f4ba9f5ef7dce078b51405
piexilili <1165893368@qq.com>
2019-10-28 17:50:46 +0800

    upgrade:更新tools.js中深拷贝函数

--- src/tools.js
+++ src/tools.js
@@ -14,6 +14,40 @@ export function checkedType(target) {
 
 /**
  *
+ * @description 深拷贝---对象/数组
+ * @export
+ * @param {object} val
+ * @returns {boolean}
+ */
+export function deepClone(target) {
+    //判断拷贝的数据类型
+    //初始化变量result 成为最终克隆的数据
+    let result, targetType = checkedType(target)
+    if (targetType === 'Object') {
+        result = {}
+    } else if (targetType === 'Array') {
+        result = []
+    } else {
+        return target
+    }
+    //遍历目标数据
+    for (let i in target) {
+        //获取遍历数据结构的每一项值。
+        let value = target[i]
+        //判断目标结构里的每一值是否存在对象/数组
+        if (checkedType(value) === 'Object' ||
+            checkedType(value) === 'Array') { //对象/数组里嵌套了对象/数组
+            //继续遍历获取到value值
+            result[i] = clone(value)
+        } else { //获取到value值是基本的数据类型或者是函数。
+            result[i] = value;
+        }
+    }
+    return result
+}
+
+/**
+ *
  * @description 判断是否数组类型
  * @export
  * @param {object} val
